9406ba438026e87454cad6bac642846eb99ca719
Ali Shakiba <a.shakiba.iran@gmail.com>
2021-08-05 12:49:12 +0430

    updating the structure and the methods

--- BucketListStructure.h
+++ BucketListStructure.h
@@ -8,43 +8,16 @@ class BucketListStructure
 private:
     int number_of_cells;
     int pmax;
-    BucketListNode** cells;
+    pair<BucketListNode*, int>* cells;
     BucketListNode** gains;
     int max_gain_index;
-public:
-    BucketListStructure(int number_of_cells, int pmax) {
-        this->number_of_cells = number_of_cells;
-        this->pmax = pmax;
-        this->cells = new BucketListNode*[number_of_cells];
-        for(int i = 0; i < this->number_of_cells; ++i) {
-            this->cells[i] = nullptr;
-        }
-        this->gains = new BucketListNode*[pmax * 2 + 1];
-        for(int i = 0; i < this->pmax * 2 + 1; ++i) {
-            BucketListNode *dummy_node = new BucketListNode(-1);
-            this->gains[i] = dummy_node;
-        }
-        this->max_gain_index = 0;
-    }
-    ~BucketListStructure() {
-        for(int i = 0; i < this->number_of_cells; ++i)
-            delete this->cells[i];
-        for(int i = 0; i < 2 * this->pmax + 1; ++i)
-            delete this->gains[i];
-        delete[] this->cells;
-        delete[] this->gains;
-    }
-    int index_to_gain(int index) {
+    int number_of_free_cells;
+protected:
+    inline int index_to_gain(int index) {
         return index - this->pmax;
     }
-    int gain_to_index(int gain) {
+    inline int gain_to_index(int gain) {
         return gain + this->pmax;
-    }
-    int max_gain_index() {
-        return this->max_gain_index;
-    }
-    int max_gain() {
-        return this->index_to_gain(this->max_gain_index);
     }
     // we'll check whether we need to update the
     // max_gain_index value
@@ -53,22 +26,6 @@ public:
             this->max_gain_index = gain_to_index(gain);
         }
     }
-    // adding a cell to front of gains list
-    void add_cell_to_front(int cell, int gain) {
-        if (this->cells[cell] != nullptr) {
-            // we had it previously, so lets just change it.
-            auto node = this->cells[cell];
-            this->remove_cell_from_gain(node, gain);
-            this->add_cell_to_front_gain(node, gain);
-        }
-        else {
-            // it was not here, so lets just add it
-            BucketListNode *node = new BucketListNode(cell);
-            this->add_cell_to_front_gain(node, gain);
-            this->cells[cell] = node;
-        }
-        this->update_max_gain_index_after_add(gain);
-    }
     // adding a cell node to front of gains list
     void add_cell_to_front_gain(BucketListNode* node, int gain) {   
         auto dummy_head = this->gains[gain];
@@ -76,32 +33,21 @@ public:
             // it is empty
             dummy_head->set_next(node);
             dummy_head->set_prev(node);
+
             node->set_next(dummy_head);
             node->set_prev(dummy_head);
         }
         else {
             // it is not empty
             auto first_node = dummy_head->get_next();
+
             node->set_next(first_node);
             node->set_prev(dummy_head);
+
             first_node->set_prev(node);
+
             dummy_head->set_next(node);
         }
-    }
-    void add_cell_to_end(int cell, int gain) {
-        if (this->cells[cell] != nullptr) {
-            // we had it previously, so lets just change it.
-            auto node = this->cells[cell];
-            this->remove_cell_from_gain(node, gain);
-            this->add_cell_to_end_gain(node, gain);
-        }
-        else {
-            // it was not here, so lets just add it
-            BucketListNode *node = new BucketListNode(cell);
-            this->add_cell_to_end_gain(node, gain);
-            this->cells[cell] = node;
-        }
-        this->update_max_gain_index_after_add(gain);
     }
     void add_cell_to_end_gain(BucketListNode* node, int gain) {   
         auto dummy_head = this->gains[gain];
@@ -109,6 +55,7 @@ public:
             // it is empty
             dummy_head->set_next(node);
             dummy_head->set_prev(node);
+
             node->set_next(dummy_head);
             node->set_prev(dummy_head);
         }
@@ -117,42 +64,10 @@ public:
             auto last_node = dummy_head->get_prev();
             node->set_prev(last_node);
             node->set_next(dummy_head);
+
             last_node->set_next(node);
+
             dummy_head->set_prev(node);
-        }
-    }
-    void remove_cell_from_gain(BucketListNode* node, int gain) {   
-        auto prev = node->get_prev();
-        auto next = node->get_next();
-        if (prev->get_cell_number() == -1 && next->get_cell_number() == -1) {
-            // we are removing from beginning of the list
-            // and the list would be empty afterwards
-            prev->set_next(nullptr);
-            prev->set_prev(nullptr);
-
-            node->set_next(nullptr);
-            node->set_prev(nullptr);
-
-            update_max_gain_index_after_remove(gain);
-        }
-        else if (prev->get_cell_number() == -1) {
-            // we are removing from beginning of the list
-            prev->set_next(next);
-            next->set_prev(prev);
-
-            node->set_next(nullptr);
-            node->set_prev(nullptr);
-        }
-        else if (next->get_cell_number() == -1) {
-            // we are removing from the end of the list
-            next->set_prev(prev);
-            prev->set_next(next);
-
-            node->set_next(nullptr);
-            node->set_prev(nullptr);
-        }
-        else {
-
         }
     }
     // precondition: the gains[gain] is of length 0 after removal of node
@@ -167,6 +82,142 @@ public:
                 }
             }
         }
+    }
+    void remove_cell_from_gain(BucketListNode* node, int gain) {   
+        auto prev = node->get_prev();
+        auto next = node->get_next();
+        if (prev->get_cell_number() == -1 && next->get_cell_number() == -1) {
+            // we are removing from beginning of the list
+            // and the list would be empty afterwards
+            // prev and next are both the dummy node
+            prev->set_next(nullptr);
+            prev->set_prev(nullptr);
+
+            node->set_next(nullptr);
+            node->set_prev(nullptr);
+
+            update_max_gain_index_after_remove(gain);
+        }
+        else if (prev->get_cell_number() == -1) {
+            // we are removing from beginning of the list
+            // prev is the dummy node
+            // and next is another standard node
+            prev->set_next(next);
+            next->set_prev(prev);
+
+            node->set_next(nullptr);
+            node->set_prev(nullptr);
+        }
+        else if (next->get_cell_number() == -1) {
+            // we are removing from the end of the list
+            // next is the dummy node and prev is another standard node
+            next->set_prev(prev);
+            prev->set_next(next);
+
+            node->set_next(nullptr);
+            node->set_prev(nullptr);
+        }
+        else {
+            // we are removing from the middle of the list
+            next->set_prev(prev);
+            prev->set_next(next);
+
+            node->set_next(nullptr);
+            node->set_prev(nullptr);
+        }
+    }
+    inline BucketListNode* get_front_cell(int gain) {
+        return this->gains[this->gain_to_index(gain)]->get_next();
+    }
+    inline BucketListNode* get_end_cell(int gain) {
+        return this->gains[this->gain_to_index(gain)]->get_prev();
+    }
+public:
+    BucketListStructure(int number_of_cells, int pmax) {
+        this->number_of_cells = number_of_cells;
+        this->pmax = pmax;
+        this->cells = new pair<BucketListNode*,int>[number_of_cells];
+        for(int i = 0; i < this->number_of_cells; ++i) {
+            this->cells[i] = make_pair(nullptr, 0);
+        }
+        this->gains = new BucketListNode*[pmax * 2 + 1];
+        for(int i = 0; i < this->pmax * 2 + 1; ++i) {
+            BucketListNode *dummy_node = new BucketListNode(-1);
+            this->gains[i] = dummy_node;
+        }
+        this->max_gain_index = 0;
+        this->number_of_free_cells = 0;
+    }
+    ~BucketListStructure() {
+        for(int i = 0; i < this->number_of_cells; ++i)
+            delete this->cells[i].first;
+        for(int i = 0; i < 2 * this->pmax + 1; ++i)
+            delete this->gains[i];
+        delete[] this->cells;
+        delete[] this->gains;
+    }
+    int max_gain_index() {
+        return this->max_gain_index;
+    }
+    int max_gain() {
+        return this->index_to_gain(this->max_gain_index);
+    }
+    // adding a cell to front of gains list
+    void add_cell_to_front(int cell, int gain) {
+        if (this->cells[cell].first != nullptr) {
+            // we had it previously, so lets just change it.
+            auto node = this->cells[cell];
+            this->remove_cell_from_gain(node.first, node.second);
+            this->add_cell_to_front_gain(node.first, gain);
+            node.second = gain;
+        }
+        else {
+            // it was not here, so lets just add it
+            BucketListNode *node = new BucketListNode(cell);
+            this->add_cell_to_front_gain(node, gain);
+            this->cells[cell] = make_pair(node, gain);
+            this->number_of_free_cells++;
+        }
+        this->update_max_gain_index_after_add(gain);
+    }
+    void add_cell_to_end(int cell, int gain) {
+        if (this->cells[cell].first != nullptr) {
+            // we had it previously, so lets just change it.
+            auto node = this->cells[cell];
+            this->remove_cell_from_gain(node.first, node.second);
+            this->add_cell_to_end_gain(node.first, gain);
+            node.second = gain;
+        }
+        else {
+            // it was not here, so lets just add it
+            BucketListNode *node = new BucketListNode(cell);
+            this->add_cell_to_end_gain(node, gain);
+            this->cells[cell] = make_pair(node, gain);
+            this->number_of_free_cells++;
+        }
+        this->update_max_gain_index_after_add(gain);
+    }
+    // int get_cell_with_max_gain()
+    int get_cell_with_max_gain_front() {
+        auto cell_node = this->get_front_cell(this->max_gain_index);
+        if (cell_node == nullptr) {
+            return -1;
+        }
+        else {
+            return cell_node->get_cell_number();
+        }
+    }
+    int get_cell_with_max_gain_end() {
+        auto cell_node = this->get_end_cell(this->max_gain_index);
+        if (cell_node == nullptr) {
+            return -1;
+        }
+        else {
+            return cell_node->get_cell_number();
+        }
+    }
+    inline bool empty() {
+        return this->number_of_free_cells == 0;
     }
 };
 
